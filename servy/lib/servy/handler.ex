915f78e133c5e41d495ef8e7954af0ef2e866630
Neil Grogan <neil@grogan.org>
2023-02-16 09:03:07 +0000

    mid way through video 5

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -23,7 +23,7 @@ defmodule Servy.Handler do
 
   def route(conv) do
     # TODO: Create a new map that also has the response body:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+    %{ conv | resp_body: "Bears, Lions, Tigers" }
   end
 
   def format_response(conv) do
