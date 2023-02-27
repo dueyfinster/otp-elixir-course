2081a44b445586de2a520c2d7e1e88aab6ab1c19
Neil Grogan <neil@grogan.org>
2023-02-27 20:57:11 +0000

    Finish video 9 - serving files

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -62,17 +62,52 @@ defmodule Servy.Handler do
 
   def route(%{ method: "GET", path: "/about" } = conv) do
     # TODO: Create a new map that also has the response body:
-    case File.read("pages/about.html") do
-      {:ok, contents} ->
-        %{conv | status: 200, resp_body: contents}
-
-      {:error, :enoent} ->
-          %{conv | status: 404, resp_body: "File not found!"}
-
-      {:error, reason} ->
-        %{conv | status: 500, resp_body: "File error: #{reason}"}
-    end
+    file =
+      Path.expand("../../pages", __DIR__)
+      |> Path.join("about.html")
+      |> File.read
+      |> handle_file(conv)
   end
+
+  def route(%{ method: "GET", path: "/pages/" <> file } = conv) do
+    # TODO: Create a new map that also has the response body:
+
+    file =
+      Path.expand("../../pages", __DIR__)
+      |> Path.join(file <> ".html")
+      |> File.read
+      |> handle_file(conv)
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!" }
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+  end
+
+  # def route(%{ method: "GET", path: "/about" } = conv) do
+  #   # TODO: Create a new map that also has the response body:
+  #   file =
+  #     Path.expand("../../pages", __DIR__)
+  #     |> Path.join("about.html")
+
+  #   case File.read(file) do
+  #     {:ok, contents} ->
+  #       %{conv | status: 200, resp_body: contents}
+
+  #     {:error, :enoent} ->
+  #         %{conv | status: 404, resp_body: "File not found!"}
+
+  #     {:error, reason} ->
+  #       %{conv | status: 500, resp_body: "File error: #{reason}"}
+  #   end
+  # end
 
   def route(%{ method: "GET", path: "/bears" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
@@ -187,6 +222,19 @@ IO.puts response
 
 request = """
 GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /pages/contact HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
