227567fa823e16a04b050e3e8f5f13976fb66034
Neil Grogan <neil@grogan.org>
2023-03-01 20:59:46 +0000

    Video 14 recursion - exercise 1

--- servy/lib/servy/recurse.ex
+++ servy/lib/servy/recurse.ex
@@ -5,7 +5,24 @@ defmodule Servy.Recurse do
   end
 
   def loopy([]), do: IO.puts "Done!"
+
+  def sum([head | tail], sum) do
+    sum = sum + head
+    sum(tail, sum)
+  end
+
+  def sum([], sum), do: sum
+
+  def triple([head | tail]) do
+    [ head * 3 | triple(tail)]
+  end
+
+  def triple([]), do: []
 end
 
 
 Servy.Recurse.loopy([1, 2, 3, 4, 5])
+
+IO.puts Servy.Recurse.sum([1, 2, 3, 4, 5], 0)
+
+IO.inspect Servy.Recurse.triple([1, 2, 3, 4, 5])
